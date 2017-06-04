aefea18127ed34cd0f09fb30743b61c907bfda34
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2017-06-04 19:10:53 +0300

    refactoring: routing changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(Router);
 
 // route-level code splitting
-const createListView = id => () => import('../views/CreateListView').then(m => m.default(id))
-const ItemView = () => import('../views/ItemView.vue')
-const UserView = () => import('../views/UserView.vue')
+const createListView = id => () => import('../views/CreateListView').then(m => m.default(id));
+const ItemView = () => import('../views/ItemView.vue');
+const UserView = () => import('../views/UserView.vue');
+const Main = () => import('../components/Main.vue');
 
-export function createRouter () {
+
+export function createRouter() {
   return new Router({
     mode: 'history',
-    scrollBehavior: () => ({ y: 0 }),
+    scrollBehavior: () => ({y: 0}),
     routes: [
-      { path: '/top/:page(\\d+)?', component: createListView('top') },
-      { path: '/new/:page(\\d+)?', component: createListView('new') },
-      { path: '/show/:page(\\d+)?', component: createListView('show') },
-      { path: '/ask/:page(\\d+)?', component: createListView('ask') },
-      { path: '/job/:page(\\d+)?', component: createListView('job') },
-      { path: '/item/:id(\\d+)', component: ItemView },
-      { path: '/user/:id', component: UserView },
-      { path: '/', redirect: '/top' }
+      {path: '/main/', },
+      {path: '/top/:page(\\d+)?', component: createListView('top')},
+      {path: '/item/:id(\\d+)', component: ItemView},
+      {path: '/user/:id', component: UserView},
+      // {path: '/', redirect: '/main'}
+      {path: '/', component: Main}
     ]
   })
 }
